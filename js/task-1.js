1bd6a5d994e8ecbca8bb86994166d7a5b1252b92
luliiaIlchenko <solnyulka@gmail.com>
2020-07-30 14:36:26 +0300

    Задача 5 (ДЗ1)

--- js/task-1.js
+++ js/task-1.js
@@ -65,7 +65,48 @@ console.log(totalPrice);*/
 
 //Ex5
 
-const countryName = "КитаЙ";
+const countryName = null;
+const CANCELED_BY_USER = "Отменено пользователем!";
+const NO_DELIVERY = "В выбранную страну доставка недоступна.";
+const CHINA = "Китай";
+const AUSTRALIA = "Австралия";
+const INDIA = "Индия";
+const JAMAICA = "Ямайка";
+let message;
+let price = 0;
+let country;
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+  switch (country) {
+    case CHINA:
+      price = 100;
+      break;
+
+    case AUSTRALIA:
+      price = 170;
+      break;
+
+    case INDIA:
+      price = 80;
+      break;
+
+    case JAMAICA:
+      price = 120;
+      break;
+
+    default:
+      message = NO_DELIVERY;
+  }
+}
+if (price > 0) {
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+
+console.log(message);
+
+/*const countryName = "КитаЙ";
 
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
@@ -77,14 +118,34 @@ let message;
 let price = 0;
 let country;
 if (countryName === null) {
-    message = CANCELED_BY_USER;
+  message = CANCELED_BY_USER;
 } else {
-    country = countryName[0] + countryName.slice(); // Write code on this line
-    switch (country) {
-        // Write code under this line
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();// Write code on this line
+  switch (country) {
+  // Write code under this line
+      case CHINA:
+      price = 100;
+      break;
 
-    }
+    case AUSTRALIA:
+      price = 170;
+      break;
+
+    case INDIA:
+      price = 80;
+      break;
+
+    case JAMAICA:
+      price = 120;
+      break;
+
+      default:
+      message = NO_DELIVERY;
+
+  }
 }
-if () { // Write code on this line
-    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+if (price > 0) { // Write code on this line
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
 }
+
+console.log(message); */
